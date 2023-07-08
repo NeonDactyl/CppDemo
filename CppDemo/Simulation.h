8e27e21a567427d01f7891fd21f9e9a22c1f5668
neondactyl <treyw05@gmail.com>
2023-07-08 13:39:28 -0500

    more updates for explanation

--- CppDemo/Simulation.h
+++ CppDemo/Simulation.h
@@ -3,11 +3,11 @@ class Simulation
 {
 private:
     int value1;
-    int value2;
     float value3;
     double value4;
     void reusableFunctionOne();
 protected:
+    int value2;
     virtual void reusableFunctionTwo();
 public:
     Simulation(int v1, int v2, float v3, double v4);
--- CppDemo/SimulationTwo.cpp
+++ CppDemo/SimulationTwo.cpp
@@ -4,6 +4,7 @@
 void SimulationTwo::reusableFunctionTwo()
 {
 	std::cout << "Booga Ooga";
+	std::cout << value2;
 }
 
 SimulationTwo::SimulationTwo(int v1, int v2, float v3, double v4) : Simulation(v1, v2, v3, v4)

e3b04eec31c73737a42b84f1ed84a7305052a9ef
neondactyl <treyw05@gmail.com>
2023-07-08 13:42:45 -0500

    one more check, done for today

--- CppDemo/SimulationTwo.cpp
+++ CppDemo/SimulationTwo.cpp
@@ -3,8 +3,8 @@
 
 void SimulationTwo::reusableFunctionTwo()
 {
-	std::cout << "Booga Ooga";
-	std::cout << value2;
+	std::cout << "Booga Ooga\n";
+	std::cout << value2 << '\n';
 }
 
 SimulationTwo::SimulationTwo(int v1, int v2, float v3, double v4) : Simulation(v1, v2, v3, v4)
